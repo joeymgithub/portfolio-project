676b478a1efce98bf6e5d2acbe62bc5d3e708d47
joeymgithub <hellojoeymillar@gmail.com>
2024-09-25 10:42:20 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const nav = document.querySelector('.landing-nav');
+    const links = nav.querySelectorAll('a');
+
+    nav.addEventListener('click', function () {
+        nav.classList.toggle('show-menu');
+    });
+
+    links.forEach(link => {
+        link.addEventListener('click', () => {
+            nav.classList.remove('show-menu'); // Close the menu when a link is clicked
+        });
+    });
+});
